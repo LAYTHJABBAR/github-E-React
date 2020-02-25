fade65f5020bfbe3a14e5058348a03747fd11684
LAYTHJABBAR <layth.jabbar@yahoo.com>
2020-02-25 08:43:37 +0000

    add useritem and user components for the github with styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import NavBar from './components/layOut/NavBar'
 import './App.css';
+import Users from "./components/Users/User.js"
+class App extends Component {
+  render() {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className="App">
+        <NavBar title = 'GitHub Engine'/>
+         <div className="container" >
+
+        <Users />
+         </div>
+
+         
+       
+      </div>
+    );
+  }
 }
 
 export default App;
