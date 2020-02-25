e3e34e0ccb882a369c5ff266556b4d9cf74fb9d0
LAYTHJABBAR <layth.jabbar@yahoo.com>
2020-02-25 17:01:12 +0000

    impelemnt the axios function to fetch the data from the github API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React, {Component} from 'react';
-import NavBar from './components/layOut/NavBar'
-import './App.css';
-import Users from "./components/Users/User.js"
-class App extends Component {
-  render() {
+import React, { Component } from "react";
+import NavBar from "./components/layOut/NavBar";
+import "./App.css";
+import Users from "./components/Users/User.js";
+import axios from "axios";
 
+class App extends Component {
+  state = {
+    users: [],
+    Loading: false
+  };
+
+  async UNSAFE_componentWillMount() {
+    this.setState({ Loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, Loading: true });
+  }
+  render() {
     return (
       <div className="App">
-        <NavBar title = 'GitHub Engine'/>
-         <div className="container" >
-
-        <Users />
-         </div>
-
-         
-       
+        <NavBar title="GitHub Engine" />
+        <div className="container">
+          <Users users={this.state.users} Loading={this.state.Loading} />
+        </div>
       </div>
     );
   }
